fb04189f60f8aa54eafa9e2232194e1b634e77e1
BuZain <buzain@gmail.com>
2012-02-25 17:18:19 +0300

    day 6 exercise commit

---
+++ my_rack_proc.rb
@@ -0,0 +1,4 @@
+my_rack_proc = lambda { |env| [200, {"Content-Type" => "text/plain"}, 
+                               ["Command line argument you typed was: #{ARGV.join(' ')} "]] }
+puts my_rack_proc.call({})
+
